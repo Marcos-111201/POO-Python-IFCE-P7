034f8719cfffe4358e65e9574dcdea4ac17d5db1
Marcos-111201 <60699196+Marcos-111201@users.noreply.github.com>
2020-02-17 17:06:12 -0300

    Atualizado em 17/02/2020

--- InserirValores.py
+++ InserirValores.py
@@ -1,6 +1,6 @@
 x = int (input("Insira um numero:"))
 y = int (input("Insira outro numero:"))
 
-print("\nO valor de x = ", x)
+print("O valor de x = ", x)
 print("O valor de y = ", y)
 print("x*y = ", x*y)
