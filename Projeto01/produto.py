cb004d9bcec26dff48b9f6f97a1fd35b451a6f42
Marcos-111201 <60699196+Marcos-111201@users.noreply.github.com>
2020-08-07 18:51:17 -0300

    Add files via upload
    Atualizado em 07/08/2020

--- Projeto01/produto.py
+++ Projeto01/produto.py
@@ -25,8 +25,6 @@ class Produto():
         string="\nId={3} Codigo={2} Descricao={1} Valor Unitario={0}".format(self._valorUnitario, self._descricao, self._codigo, self._id)
         return string
     
-   
-    
 if __name__ == '__main__':
     produto=Produto(1,100,'Arroz', 5.5)        
     print(produto.str())
